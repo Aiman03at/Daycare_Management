2a2efe5c97f73edd5affb3786387ce5615ec1575
Aiman Mushtaq <aiman03at@gmail.com>
2026-05-20 18:46:09 -0400

    Add some fixes

--- daycare-frontend/src/api/client.ts
+++ daycare-frontend/src/api/client.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { clearAuthSession, getToken } from "../auth/session";
 
-export const API_BASE_URL = "http://localhost:4000/api";
+export const API_BASE_URL = import.meta.env.VITE_API_URL
+  ? `${import.meta.env.VITE_API_URL}/api`
+  : "http://localhost:4000/api";
+
 export const BACKEND_BASE_URL = API_BASE_URL.replace(/\/api$/, "");
 
 export const api = axios.create({
